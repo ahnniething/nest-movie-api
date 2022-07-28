8b6e7764724ca57bc4a84c732f1b3f0d6292ea7b
ahnniething <ahnniething@gmail.com>
2022-07-28 12:01:44 +0900

    test: add comments

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -21,7 +21,7 @@ describe('AppController (e2e)', () => {
     );
     await app.init();
   });
-
+  // access url
   it('/ (GET)', () => {
     return request(app.getHttpServer())
       .get('/')
@@ -30,11 +30,12 @@ describe('AppController (e2e)', () => {
   });
 
   describe('/movies', () => {
-    it('(GET)', () => {
+    // read movies
+    it('(GET) 200', () => {
       return request(app.getHttpServer()).get('/movies').expect(200).expect([]);
     });
-
-    it('(POST)', () => {
+    // create a movie
+    it('(POST) 201', () => {
       return request(app.getHttpServer())
         .post('/movies')
         .send({
@@ -44,20 +45,45 @@ describe('AppController (e2e)', () => {
         })
         .expect(201);
     });
-
-    it('(DELETE)', () => {
+    // try to create a movie with a wrong parameter
+    it('(POST) 400', () => {
+      return request(app.getHttpServer())
+        .post('/movies')
+        .send({
+          title: 'Test',
+          year: 2000,
+          genres: ['test'],
+          other: 'thing',
+        })
+        .expect(400);
+    });
+    // try to delete all movies
+    it('(DELETE) 404', () => {
       return request(app.getHttpServer()).delete('/movies').expect(404);
     });
   });
 
   describe('/movies/:id', () => {
+    // read a movie that create for testing at line 38
     it('(GET) 200', () => {
       return request(app.getHttpServer()).get('/movies/1').expect(200);
     });
+    // try to read a movie with doesn't exist
     it('(GET) 404', () => {
       return request(app.getHttpServer()).get('/movies/999').expect(404);
     });
-    it.todo('DELETE');
-    it.todo('PATCH');
+    // update a movie title
+    it('(PATCH) 200', () => {
+      return request(app.getHttpServer())
+        .patch('/movies/1')
+        .send({
+          title: 'Updated Test',
+        })
+        .expect(200);
+    });
+    // delete a movie
+    it('(DELETE) 200', () => {
+      return request(app.getHttpServer()).delete('/movies/1').expect(200);
+    });
   });
 });
